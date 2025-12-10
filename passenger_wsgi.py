312c7875c4483595995aeca8e4dc139f39ba79f0
Amarramitha <amarra.mitha@gmail.com>
2025-12-10 15:40:36 +0700

    update wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,15 @@
 import sys
 import os
 
-# Tambahkan path project ke sys.path
-sys.path.insert(0, os.path.dirname(__file__))
+# Path project yang BENAR
+project_home = '/home/lath1977/public_html/sispakapp'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
+# Aktifkan virtualenv yang BENAR
+activate_this = '/home/lath1977/public_html/sispakapp/venv/bin/activate_this.py'
+with open(activate_this) as f:
+    exec(f.read(), {'__file__': activate_this})
+
+# Import Flask app
 from app import app as application
